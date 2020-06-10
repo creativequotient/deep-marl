d3c3c34ff6dcf7359ca0d652bb085d2930d07ebe
Marcus Lee <marcus_lee@u.nus.edu>
2020-06-10 19:38:45 +0800

    Experiment script fix

--- experiments/experiments_m3ddpg.sh
+++ experiments/experiments_m3ddpg.sh
@@ -3,28 +3,33 @@
 # Hyperparameters
 
 runs=$(seq 0 10)
-perturbation_factors=(0.0 0.1 0.01 0.001)
+perturbation_factors=(0.1 0.01)
 
 echo ${runs[@]}
 echo ${perturbation_factors[@]}
 
-# Run experiments
+if [ $1 == 'gcp' ]
+then
+  echo "Running gcp set"
 
-parallel -j 5 "python ../main.py --scenario simple_adversary --discrete --good-policy m3ddpg --adv-policy {1} --save-dir simple_adversary/{1}-vs-m3ddpg-run-{3}_perturbation-{2} --perturbation {2}" ::: ddpg m3ddpg ::: ${perturbation_factors[@]} ::: ${runs[@]}
+  parallel -j 5 "python ../main.py --scenario simple_adversary --num-adversaries 1 --discrete --good-policy m3ddpg --adv-policy {1} --save-dir simple_adversary/{1}-vs-m3ddpg-run-{3}_perturbation-{2} --perturbation {2}" ::: ddpg m3ddpg ::: ${perturbation_factors[@]} ::: ${runs[@]}
 
-parallel -j 5 "python ../main.py --scenario simple_push --discrete --good-policy m3ddpg --adv-policy {1} --save-dir simple_push/m3ddpg-vs-{1}-run-{3}_perturbation-{2} --perturbation {2}" ::: ddpg m3ddpg ::: ${perturbation_factors[@]} ::: ${runs[@]}
+  parallel -j 5 "python ../main.py --scenario simple_push --num-adversaries 1 --discrete --good-policy m3ddpg --adv-policy {1} --save-dir simple_push/m3ddpg-vs-{1}-run-{3}_perturbation-{2} --perturbation {2}" ::: ddpg m3ddpg ::: ${perturbation_factors[@]} ::: ${runs[@]}
 
-parallel -j 5 "python ../main.py --scenario simple_push --discrete --good-policy {1} --adv-policy m3ddpg --save-dir simple_push/{1}-vs-m3ddpg-run-{3}_perturbation-{2} --perturbation {2}" ::: ddpg m3ddpg ::: ${perturbation_factors[@]} ::: ${runs[@]}
+  parallel -j 5 "python ../main.py --scenario simple_push --num-adversaries 1 --discrete --good-policy {1} --adv-policy m3ddpg --save-dir simple_push/{1}-vs-m3ddpg-run-{3}_perturbation-{2} --perturbation {2}" ::: ddpg m3ddpg ::: ${perturbation_factors[@]} ::: ${runs[@]}
 
-parallel -j 5 "python ../main.py --scenario simple_speaker_listener --discrete --good-policy m3ddpg --save-dir simple_speaker_listener/m3ddpg-run-{2}_perturbation-{1} --perturbation {1}" ::: ${perturbation_factors[@]} ::: ${runs[@]}
+  parallel -j 5 "python ../main.py --scenario simple_speaker_listener --discrete --good-policy m3ddpg --save-dir simple_speaker_listener/m3ddpg-run-{2}_perturbation-{1} --perturbation {1}" ::: ${perturbation_factors[@]} ::: ${runs[@]}
+else
+  echo "Running local set"
 
-parallel -j 5 "python ../main.py --scenario simple_spread --discrete --good-policy m3ddpg --save-dir simple_spread/m3ddpg-run-{2}_perturbation-{1} --perturbation {1}" ::: ${perturbation_factors[@]} ::: ${runs[@]}
+  parallel -j 5 "python ../main.py --scenario simple_spread --discrete --good-policy m3ddpg --save-dir simple_spread/m3ddpg-run-{2}_perturbation-{1} --perturbation {1}" ::: ${perturbation_factors[@]} ::: ${runs[@]}
 
-parallel -j 5 "python ../main.py --scenario simple_spread --discrete --good-policy m3ddpg --save-dir simple_spread_128_units/m3ddpg-run-{2}_perturbation-{1} --perturbation {1} --num-units 128" ::: ${perturbation_factors[@]} ::: ${runs[@]}
+  parallel -j 5 "python ../main.py --scenario simple_spread --discrete --good-policy m3ddpg --save-dir simple_spread_128_units/m3ddpg-run-{2}_perturbation-{1} --perturbation {1} --num-units 128" ::: ${perturbation_factors[@]} ::: ${runs[@]}
 
-parallel -j 5 "python ../main.py --scenario simple_tag --discrete --good-policy {1} --adv-policy m3ddpg --save-dir simple_tag/{1}-vs-m3ddpg-run-{3}_perturbation-{2} --perturbation {2}" ::: ddpg m3ddpg ::: ${perturbation_factors[@]} ::: ${runs[@]}
+  parallel -j 5 "python ../main.py --scenario simple_tag --num-adversaries 3 --discrete --good-policy {1} --adv-policy m3ddpg --save-dir simple_tag/{1}-vs-m3ddpg-run-{3}_perturbation-{2} --perturbation {2}" ::: ddpg m3ddpg ::: ${perturbation_factors[@]} ::: ${runs[@]}
 
-parallel -j 5 "python ../main.py --scenario simple_tag --discrete --good-policy {1} --adv-policy m3ddpg --save-dir simple_tag_128_units/{1}-vs-m3ddpg-run-{3}_perturbation-{2} --perturbation {2} --num-units 128" ::: ddpg m3ddpg ::: ${perturbation_factors[@]} ::: ${runs[@]}
+  parallel -j 5 "python ../main.py --scenario simple_tag --num-adversaries 3 --discrete --good-policy {1} --adv-policy m3ddpg --save-dir simple_tag_128_units/{1}-vs-m3ddpg-run-{3}_perturbation-{2} --perturbation {2} --num-units 128" ::: ddpg m3ddpg ::: ${perturbation_factors[@]} ::: ${runs[@]}
+fi
 
 # Run analysis
 
